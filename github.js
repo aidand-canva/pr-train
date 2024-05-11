fa0ef53d4873e9417fd7a2fd785368b912adca8b
Aidan Do <aidand@canva.com>
2024-05-11 20:50:19 +0930

    put train on right

--- github.js
+++ github.js
@@ -43,7 +43,7 @@ function constructTrainNavigation(branchToPrDict, currentBranch, combinedBranch)
     const prInfoHtml = `1. ${prNumber}${combinedInfo}${maybeHandRight}`;
     prList.push(prInfoHtml);
   });
-  contents += '### 🚂 PR Train \n';
+  contents += '### PR Train 🚂 \n';
   contents += prList.join('\n');
   contents += '\n</pr-train-toc>'
   return contents;
